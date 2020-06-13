a3020a52b7927d5e07015583c6874d39a9f968d9
junhong0113 <junhong.cfa@gmail.com>
2020-06-13 17:59:16 +0800

    all done with description

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -1,12 +1,12 @@
 function palindrome(str){
-    let regex = /[a-z0-9]+/ig;
-    let newstr = str.match(regex).join("").toLowerCase();
+    let regex = /[a-z0-9]+/ig; //Create a regular expression to get only alphanumeric character
+    let newstr = str.match(regex).join("").toLowerCase(); //Get a clean string with lower case
+    
+    //below code create a backward string
     let backstrarr = [];
-
     for (let i = newstr.length - 1; i >= 0; i--)
         {backstrarr.push(newstr[i]);}
-
     let backstr = backstrarr.join("");
-
-    return newstr === backstr? true:false;
+    //end of above code
+    return newstr === backstr? true:false; //compare original string and its backward string, checker done.
 }
